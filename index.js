b6ba1410ad939b7d2e65501cb82d44dcbcec5db8
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-22 23:12:44 +0600

    feat: implement MongoDB connection and add API endpoint for ebooks

--- index.js
+++ index.js
@@ -9,8 +9,45 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Foo, Bar!')
 })
+
+const uri = process.env.MONGO_DB_URI;;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("fable_db");
+    const ebooksCollection = db.collection("ebooks");
+
+
+
+    app.get('/api/ebooks', async (req, res) => {
+        const ebooks = await ebooksCollection.find().toArray();
+        res.json(ebooks);   
+    });
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
