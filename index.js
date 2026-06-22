162d65aa7a5e8c3ef8cecb2e6c112f5c312e9883
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-22 22:55:13 +0600

    chore: add package.json with initial dependencies and configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = 8080
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
